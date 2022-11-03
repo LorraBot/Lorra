d543d48085c282f4d3cea3d2467a2f0eded0b2e3
OoP1nk <per.nmds@gmail.com>
2022-11-03 20:10:50 +0000

    Fixed SlashCommand Subcommand/SubcommandGroup loading

--- src/structures/interactions/slashcommand/SlashCommand.ts
+++ src/structures/interactions/slashcommand/SlashCommand.ts
@@ -9,7 +9,7 @@ import ExecutableCommand from "../ExecutableCommand";
 import SlashCommandSubCommand from "./SlashCommandSubCommand";
 
 export default abstract class SlashCommand extends ExecutableCommand {
-    private commandData: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">|null = null;
+    private commandData: SlashCommandBuilder|null = null;
     private subcommands: Array<SlashCommandSubCommand> = Array.of();
     private subcommandGroups: Collection<SlashCommandSubcommandGroupBuilder, Array<SlashCommandSubCommand>> = new Collection();
 
@@ -19,11 +19,23 @@ export default abstract class SlashCommand extends ExecutableCommand {
 
     public execute(client: Lorra, interaction: ChatInputCommandInteraction): void {}
 
-    public getSlashCommandData(): Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> {
+    public getSlashCommandData(): SlashCommandBuilder {
+        if(this.subcommandGroups) {
+            this.subcommandGroups.forEach((value, key) => {
+                value.forEach((cmd) => {
+                    key.addSubcommand(cmd.getSubcommandData());
+                });
+                this.commandData?.addSubcommandGroup(key);
+            });
+        } else {
+            this.subcommands.forEach((value) => {
+                this.commandData?.addSubcommand(value.getSubcommandData());
+            });
+        }
         return this.commandData!;
     }
 
-    public setSlashCommandData(commandData: Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">): void {
+    public setSlashCommandData(commandData: SlashCommandBuilder): void {
         this.commandData = commandData;
     }
 
