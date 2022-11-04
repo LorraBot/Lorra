bd97b727ba054325e3907c65e9452cc2dd5a85da
OoP1nk <per.nmds@gmail.com>
2022-11-04 14:45:31 +0000

    [~Changed] Meme command to use redit instead of tennor

--- src/systems/fun/MemeCommand.ts
+++ src/systems/fun/MemeCommand.ts
@@ -1,36 +1,42 @@
 import client from "../../client/client";
-import { ChatInputCommandInteraction, CacheType, EmbedBuilder } from "discord.js";
+import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import { SlashCommand } from "../../structures";
 import Commands from "../../util/Commands";
 import { default as axios } from 'axios';
-import ms from "ms";
+import { BotColor } from "../../util/Enums";
 
 export default class MemeCommand extends SlashCommand {
-    private _results: any;
-    private _timeout: number|null = null;
-
     constructor() {
         super();
         this.setSlashCommandData(Commands.slash('meme', 'Memeeeeessss'));
     }
 
     public async execute(client: client, interaction: ChatInputCommandInteraction) {
-        await interaction.deferReply();
-        const catagories = ["programming", "relatable", "funny", "cat", "dog", "television"];
-        const baseUrl = "https://tenor.googleapis.com/v2/search";
-        var catagory = catagories[Math.floor(Math.random() * catagories.length)];
-        if(!this._timeout || this._timeout > Date.now()) {
-            this._results = await axios.get(baseUrl, { params: { q: catagory + " memes", key: process.env.TENOR__API, limit: 64 } })
-                .then((res) => res.data.results);
-            this._timeout = Date.now() + ms('5m');
-        }
-        const obj = this._results[Math.floor(Math.random() * this._results.length)];
-        var gif = obj.media_formats.gif.url;
-        const response = new EmbedBuilder()
-            .setColor(interaction.user.hexAccentColor || 'Random')
-            .setTitle(obj.content_description)
-            .setURL(gif)
-            .setImage(gif)
-        return await interaction.followUp({ embeds: [response] });
+        const baseUrl = "https://reddit.com/r/memes/random/.json";
+        const Memebed = new EmbedBuilder().setColor(BotColor.Invisible);
+        await axios.get(baseUrl)
+        .then(async (res) => {
+            const [list] = res.data;
+            const [post] = list.data.children;
+
+            var memeTitle = post.data.title;
+            var memeSubreddit = post.data.subreddit_name_prefixed;
+            var memeUrl = `https://reddit.com${post.data.permalink}`;
+            var memeImage = post.data.url;
+            var memeUpvotes = post.data.ups;
+            var memeNumComments = post.data.num_comments;
+
+            Memebed.setTitle(`${memeTitle}`)
+                .setAuthor({ name: memeSubreddit })
+                .setURL(memeUrl)
+                .setColor('Random')
+                .setFooter({ text: `⬆️${memeUpvotes} | ${memeNumComments} Comments` })
+                .setImage(memeImage)
+                .setTimestamp()
+            await interaction.reply({ embeds: [Memebed] })
+        }).catch(async (reason) => await interaction.reply({ 
+            embeds: [Memebed.setTitle('Error Occured').setDescription(reason)], 
+            ephemeral: true 
+        }));
     }
 }
