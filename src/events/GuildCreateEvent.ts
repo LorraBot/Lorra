972d2bbba488e78c73486dabc4519071e0d96ef0
OoP1nk <per.nmds@gmail.com>
2022-11-06 21:25:42 +0000

    [+Added] GuildCreate logging for main server

--- src/events/GuildCreateEvent.ts
+++ src/events/GuildCreateEvent.ts
@@ -1,6 +1,8 @@
 import Lorra from "../client/Lorra";
-import { Events, Guild } from "discord.js";
+import { EmbedBuilder, Events, Guild } from "discord.js";
 import { EventBase } from "../structures";
+import ms from "ms";
+import { stripIndent } from "common-tags";
 
 export default class GuildCreateEvent extends EventBase {
     constructor() {
@@ -8,6 +10,22 @@ export default class GuildCreateEvent extends EventBase {
     }
 
     async execute(client: Lorra, guild: Guild) {
-        client.guildSettingsManager.addGuild(guild);
+        await client.guildSettingsManager.addGuild(guild);
+
+        var guildOwner = await guild.fetchOwner();
+        const guildLog = new EmbedBuilder()
+            .setTitle('Guild Added')
+            .setAuthor({ name: guildOwner.user.tag, iconURL: guildOwner.displayAvatarURL() })
+            .setThumbnail(guild.iconURL()!)
+            .setTimestamp()
+            .setColor('Green')
+            .setDescription(stripIndent`
+            **Name** ◦ ${guild.name}
+            **Member Count** ◦ ${guild.memberCount}
+            **CreatedAt** ◦ ${guild.createdAt.toUTCString()}
+            **Verified/Partnered** ◦ ${guild.verified ? "Yes" : "No"}/${guild.partnered ? "Yes" : "No"}
+            `)
+        const channel = client.guilds.cache.get(process.env.TEST__GUILD!)?.channels.cache.find((c) => c.name.includes("guild-log"));
+        if(channel && channel.isTextBased()) {channel.send({ embeds: [guildLog] })}
     }
 }
